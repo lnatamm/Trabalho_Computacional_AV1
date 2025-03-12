ed978d67483c98e366e017bf04e764ba4e62944c
lnatamm <lnatamm25@gmail.com>
2025-03-12 16:44:41 -0300

    Adicionando todos arquivos iniciais

---
+++ regressao.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+atividade_enzimatica = np.loadtxt("atividade_enzimatica.csv", delimiter=",")
+
+fig1 = plt.figure(1)
+plot1 = fig1.add_subplot(projection='3d')
+
+plot1.scatter(
+    atividade_enzimatica[:,0], atividade_enzimatica[:,1], atividade_enzimatica[:,2],
+    c="teal",
+    edgecolors="k"
+)
+
+plot1.set_xlabel("Temperatura")
+plot1.set_ylabel("pH da solução")
+plot1.set_zlabel("Atividade Enzimática")
+plot1.set_title("Nível de Atividade Enzimática")
+
+X = np.vstack(
+    (atividade_enzimatica[:,0], atividade_enzimatica[:,1])
+)
+
+print(X)
+
+plt.show()
