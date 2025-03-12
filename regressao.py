430c320f4f829e43f654a885677160cc1fabef73
lnatamm <lnatamm25@gmail.com>
2025-03-12 17:41:54 -0300

    Adicionando funções utilitarias e MQO Tradicional

--- regressao.py
+++ regressao.py
@@ -3,22 +3,30 @@ import matplotlib.pyplot as plt
 
 # Tarefa de Regressão
 
+# Configuração padrão dos plots
+def get_plot_configuration(file, n_figure):
+    fig = plt.figure(n_figure)
+    plot = fig.add_subplot(projection='3d')
+
+    plot.scatter(
+        file[:, 0],
+        file[:, 1],
+        file[:, 2],
+        color='teal',
+        edgecolors='k'
+    )
+
+    plot.set_xlabel("Temperatura")
+    plot.set_ylabel("pH da solução")
+    plot.set_zlabel("Nível de Atividade Enzimática")
+    plot.set_title("Atividade Enzimática")
+
+    return plot
+
 # 1.
 atividade_enzimatica = np.loadtxt("atividade_enzimatica.csv", delimiter=",")
 
-fig1 = plt.figure(1)
-plot1 = fig1.add_subplot(projection='3d')
-
-plot1.scatter(
-    atividade_enzimatica[:,0], atividade_enzimatica[:,1], atividade_enzimatica[:,2],
-    c="teal",
-    edgecolors="k"
-)
-
-plot1.set_xlabel("Temperatura")
-plot1.set_ylabel("pH da solução")
-plot1.set_zlabel("Atividade Enzimática")
-plot1.set_title("Nível de Atividade Enzimática")
+plot_1 = get_plot_configuration(atividade_enzimatica, 1)
 
 # 2.
 X = atividade_enzimatica[:,:2]
@@ -26,4 +34,22 @@ X = atividade_enzimatica[:,:2]
 y = atividade_enzimatica[:,-1]
 
 # 3.
+
+# MQO Tradicional
+
+plot_MQO_tradicional = get_plot_configuration(atividade_enzimatica, 2)
+
+x_axis = np.linspace(-4, 6, 100)
+y_axis = np.linspace(-9, 9, 100)
+X3d, Y3d = np.meshgrid(x_axis, y_axis)
+
+X_MQO_tradicional = np.hstack((
+    np.ones((X.shape[0], 1)), X
+))
+B_MQO_tradicional = np.linalg.pinv(X_MQO_tradicional.T@X_MQO_tradicional)@X_MQO_tradicional.T@y
+
+Z_MQO_tradicional = B_MQO_tradicional[0] + B_MQO_tradicional[1]*X3d + B_MQO_tradicional[2]* Y3d
+
+plot_MQO_tradicional.plot_surface(X3d, Y3d, Z_MQO_tradicional, cmap='gray')
+
 plt.show()
