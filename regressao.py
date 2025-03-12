83194fbf29b2a0990d39d701820567e2fca614e8
lnatamm <lnatamm25@gmail.com>
2025-03-12 17:00:20 -0300

    Fazendo ajustes no ponto 2

--- regressao.py
+++ regressao.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Tarefa de Regressão
+
+# 1.
 atividade_enzimatica = np.loadtxt("atividade_enzimatica.csv", delimiter=",")
 
 fig1 = plt.figure(1)
@@ -17,10 +20,10 @@ plot1.set_ylabel("pH da solução")
 plot1.set_zlabel("Atividade Enzimática")
 plot1.set_title("Nível de Atividade Enzimática")
 
-X = np.vstack(
-    (atividade_enzimatica[:,0], atividade_enzimatica[:,1])
-)
+# 2.
+X = atividade_enzimatica[:,:2]
 
-print(X)
+y = atividade_enzimatica[:,-1]
 
+# 3.
 plt.show()
